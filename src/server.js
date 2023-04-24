59801c86061e0e4c658362f5a5769c8d7083c9aa
pedro <pedroivomonte@gmail.com>
2023-04-24 17:09:34 -0300

    update: added pseudodatabase

--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 import http from "http";
 import { json } from "./middlewares/json.js";
+import Database from "./database.js";
+
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   await json(req, res);
 
+  if (method === "GET" && url === "/users") {
+    const users = database.select("users");
+    return res.end(JSON.stringify(users));
+  }
+
   if (method === "POST" && url === "/users" && req.body) {
-    const { name, body } = req.body;
+    const { name, email } = req.body;
+
+    const user = {
+      id: 1,
+      name,
+      email,
+    };
+
+    database.insert("users", user);
   }
 
   return res.writeHead(201).end();
